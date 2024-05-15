f963e1dc1f84bb5139befd500271aa3188198fe2
sil <silpilotta@gmail.com>
2024-05-15 18:57:27 -0300

    ejercicio clase 5

---
+++ clase_5/multiplosMetodo.ts
@@ -0,0 +1,9 @@
+function esMultiplo (numero1: number, numeroMultiplo: number) : boolean{
+
+    return numero1 % numeroMultiplo === 0 ;
+
+}
+let multiplo: boolean = esMultiplo(16,8)
+    console.log(multiplo)
+        
+
